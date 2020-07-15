e97c3e7a6ab651fd83f91c63709b3786d9304687
garen <garen2994@hotmail.com>
2020-07-15 22:38:49 +0800

    :star: project initialize

---
+++ src/main/java/com/garen/community/SimCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.garen.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimCommunityApplication.class, args);
+	}
+
+}
